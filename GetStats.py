a22fa9f85ce648e83537e7e39f2a63dcd64001d9
Zack <thexerronn@gmail.com>
2025-03-24 11:51:57 -0400

    Updated to new match history v2 endpoint

--- GetStats.py
+++ GetStats.py
@@ -1,4 +1,6 @@
 import json
+
+import time
 import requests
 
 API = "https://marvelrivalsapi.com"
@@ -8,11 +10,13 @@ def UpdatePlayerData(username):
     endpoint = f"{API}/api/v1/player/{username}/update"
     headers = {'x-api-key': KEY}
     request = requests.get(endpoint, headers=headers)
+    print(request.json())
     return request.json().get("success", False)
 
-def QueryMatchHistory(username, season, skip):
+def QueryMatchHistory(username, season, page):
     #game mode 2 is comp
-    endpoint = f"{API}/api/v1/player/{username}/match-history?season={season}&skip={skip}&game_mode=2"
+    endpoint = f"{API}/api/v2/player/{username}/match-history?season={season}&page={page}&game_mode=2"
+    print(endpoint)
     headers = {'x-api-key': KEY}
     request = requests.get(endpoint, headers=headers)
     return request.json()
@@ -22,15 +26,14 @@ def QueryAllMatchHistory(username):
     #      raise Exception("Failed to update player data")
     allMatches = []
     for season in [0, 1, 1.5]:
-        matchCounter = 0
+        pageCounter = 0
         while True:
-            data = QueryMatchHistory(username, season, matchCounter)
-            print(data)
+            data = QueryMatchHistory(username, season, pageCounter)
             allMatches.extend(data["match_history"])
             print(len(allMatches))
-            if len(data["match_history"]) < 20:
+            if not data["pagination"]["has_more"]:
                 break
-            matchCounter += 20
+            pageCounter += 1
     with open(f"data/{username}.json", 'w') as inf:
         outData = {"match_history": allMatches}
         inf.write(json.dumps(outData, indent=4))
@@ -56,9 +59,18 @@ def QueryAllMatchData(username):
 
     allMatches = []
     for i, match in enumerate(data["match_history"]):
-       print(i)
-       matchData = QueryMatchData(match["match_uid"])
-       allMatches.append(matchData["match_details"])
+        retries = 5
+        print(i)
+        while retries > 0:
+            try:
+                time.sleep(0.5)
+                matchData = QueryMatchData(match["match_uid"])
+                allMatches.append(matchData["match_details"])
+                break
+            except:
+                retries -= 1
+                print("Retrying...")
+                time.sleep(5)
 
     with open(f"data/{username}_all_competitive.json", 'w') as inf:
         outData = {"match_history": allMatches}
